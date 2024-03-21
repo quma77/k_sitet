909c65c616e0006cda2a949e9939545e0f3849f7
quma77 <122120440+quma77@users.noreply.github.com>
2024-03-22 02:47:10 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+document
+  .querySelector(".menu_nav .dropdown a")
+  .addEventListener("click", function () {
+    var dropdownMenu = document.querySelector(".dropdown-menu");
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
+  });
+
+document.addEventListener("click", function (event) {
+  var dropdownMenu = document.querySelector(".dropdown-menu");
+  var dropdownToggle = document.querySelector(".menu_nav .dropdown a");
+
+  if (
+    event.target !== dropdownMenu &&
+    event.target !== dropdownToggle &&
+    !dropdownMenu.contains(event.target)
+  ) {
+    dropdownMenu.style.display = "none";
+  }
+});
