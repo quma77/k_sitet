6179dcf3ca03a7384c76adaec968d60898dd109b
quma77 <122120440+quma77@users.noreply.github.com>
2024-03-25 06:06:59 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,4 +23,48 @@ document.addEventListener("click", function (event) {
 
 
 
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5 // Порог видимости элемента
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const counters = entry.target.querySelectorAll('.num_stat');
+      counters.forEach(counter => {
+        const updateCount = () => {
+          const target = +counter.getAttribute('data-count');
+          const count = +counter.innerText;
+          
+          const speed = 500; // Скорость анимации (в миллисекундах)
+          const inc = target / speed;
+          
+          if (count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 50);
+          } else {
+            counter.innerText = target;
+          }
+        };
+        
+        updateCount();
+      });
+
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+const main4Element = document.querySelector('.main_4');
+observer.observe(main4Element);
+
+
+
+
+
+
+
+
 
